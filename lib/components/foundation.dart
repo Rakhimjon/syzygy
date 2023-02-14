ad97e26a4231eb7bb8e10035a6c0ab753a86320b
Rahim Abdullayev <abdurahim21kji@gmail.com>
2023-02-14 17:29:04 +0500

    flame_game

---
+++ lib/components/foundation.dart
@@ -0,0 +1,7 @@
+
+import 'package:flame/components.dart';
+
+class Foundation extends PositionComponent {
+  @override
+  bool get debugMode => true;
+}
---
+++ lib/components/pile.dart
@@ -0,0 +1,8 @@
+
+
+import 'package:flame/components.dart';
+
+class Pile extends PositionComponent {
+  @override
+  bool get debugMode => true;
+}
